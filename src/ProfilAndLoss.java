17bd8b3d966679b5ac17bcfd71e12064cf4cb5ec
KshtriyaLokesh <ls1229921@gmail.com>
2025-11-24 21:35:22 +0530

    adding profit & loss

--- src/ProfilAndLoss.java
+++ src/ProfilAndLoss.java
@@ -4,15 +4,18 @@ import java.util.Scanner;
 public class ProfilAndLoss {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Cost Price : ");
         int cp =  sc.nextInt();
+        System.out.print("Enter Selling Price : ");
         int sp = sc.nextInt();
         if(cp>sp){
-            System.out.println("It's loss");
-            System.out.println((cp - sp) * 100 / cp);
+            System.out.print("It's loss, it is : ");
+            System.out.println((cp - sp) * 100 / cp + "%");
         }
-        else {
-            System.out.println("It's Profit");
-            System.out.println((sp - cp) * 100 / sp);
+        else if(cp<sp){
+            System.out.print("It's Profit, it is : ");
+            System.out.println((sp - cp) * 100 / sp + "%");
         }
+        else System.out.println("no profit, no loss");
     }
 }
